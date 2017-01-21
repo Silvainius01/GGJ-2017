5ee677b098e634ec889bebf7e2b23b7a151913cd
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2017-01-21 12:22:27 -0800

    Started Construction UI

---
+++ Assets/Scripts/UI/AssignmentUiManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AssignmentUiManager : MonoBehaviour {
+
+    public static AssignmentUiManager Instance;
+
+    public List<ConstructionButton> ConstructButtons = new List<ConstructionButton>();
+
+    bool Active = false;
+    GameObject Tile;
+    
+    void Awake()
+    {
+        if(AssignmentUiManager.Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void SetConstruct (GameObject _construction)
+    {
+        // assign a construct to a space
+    }
+
+    //public void ActivateUI (GameObject _tile, List<ConstructionButton> _constructionOptions)
+}
--- Assets/Scripts/UI/TileSelectionGeneration.cs
+++ Assets/Scripts/UI/TileSelectionGeneration.cs
@@ -4,16 +4,27 @@ using System.Collections.Generic;
 
 public class TileSelectionGeneration : MonoBehaviour {
 
-    public List<Transform> UiOptions;
+    public GameObject Canvas;
+    public GameObject TileUiPrefab;
     public GraphMaker My_GraphMaker;
+
+    private List<Transform> UiOptions;
+    private const float uiDistanceFromScene = -3;
 
 	// Use this for initialization
 	void Start () {
-	
+	    for(int i = 0; i < My_GraphMaker.graphPoints.Count; i++)
+        {
+            SpawnUiElement(My_GraphMaker.graphPoints[i]);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void SpawnUiElement(GraphMaker.GraphPoint _graphPoint)
+    {
+        Vector3 SpawnPoint = new Vector3(_graphPoint.pos.x, _graphPoint.pos.y, uiDistanceFromScene);
+        GameObject _UiElement = (GameObject) Instantiate(TileUiPrefab, SpawnPoint, Quaternion.identity);
+        _UiElement.transform.SetParent(Canvas.transform);
+
+        //TODO: make new Ui element know about its graph point
+    }
 }
