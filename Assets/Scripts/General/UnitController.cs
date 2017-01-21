970b23b655d73d288690941c8fa15d7ff4d97f9d
GilyeatArthur <gilyeatarthur@hotmail.com>
2017-01-21 15:03:20 -0800

    Started my own miserable piece of code

---
+++ Assets/Scripts/General/UnitController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour {
+
+    public GraphMaker graph;
+
+    void Start()
+    {
+        //graph = GraphMaker.
+
+    }
+
+  void update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //graph.N
+            graph.GetClosestPointTo(Input.mousePosition);
+
+        }
+        
+    }
+
+}
