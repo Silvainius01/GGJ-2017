c78e3c69ad998f8c993ee82c5dfa85591b2b20fb
christinejohnson <christinejohnson@nevada.unr.edu>
2017-01-21 15:52:19 -0800

    finished duelist

--- Assets/Scripts/Traps/Duelist.cs
+++ Assets/Scripts/Traps/Duelist.cs
@@ -4,18 +4,48 @@ using System.Collections;
 public class Duelist : Trap {
 
 	public float winChance = 0.8f;
-	bool firstKill = true;
+	private bool firstKill = true;
+	public Timer dualTimer = new Timer(1.0f);
+	private BasicEnemyUnit target;
 
-	// Update is called once per frame
-	void Update () {
-
+	public void Init(float winChance){
+		this.winChance = winChance;
 	}
 
-	public override void ApplyTriggerEffect (){
-		
+	// Update is called once per frame
+	public virtual void Update () {
+		if (dualTimer.isActive ()) {
+			if (dualTimer.Update (Time.deltaTime)) {
+				Duel (target);
+			}
+		} else {
+			target = FindTargetToDuel ();
+			if (target != null) {
+				// begin duel timer with enemy
+				dualTimer.Activate();
+			}
+		}
+	}
+
+	private BasicEnemyUnit FindTargetToDuel(){
+		// begin spilling the tea into the streets!!!
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("BasicEnemy");
+		foreach (GameObject enemy in enemies) {
+			// check to see if enemy is in affected grid pos
+			if(graph.IsPosInGridPos(enemy.transform.position, gridX, gridY)){
+				return enemy.GetComponent<BasicEnemyUnit> ();
+			}
+		}
+		return null;
 	}
 
 	bool Duel(BasicEnemyUnit obj){
-		return true;
+		if (UnityEngine.Random.Range (0.0f, 1.0f) >= winChance || firstKill) {
+			firstKill = false;
+			Destroy (obj);
+		} else {
+			Destroy (gameObject);
+		}
+		target = null;
 	}
 }
