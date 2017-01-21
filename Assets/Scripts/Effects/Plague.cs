b72b3c6f42216b72fee358b91157608cefd5c048
christinejohnson <christinejohnson@nevada.unr.edu>
2017-01-21 15:05:29 -0800

    changed gyro to use grid x y pos for target

---
+++ Assets/Scripts/Effects/Plague.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plague : MonoBehaviour {
+
+	private Timer deathTimer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (deathTimer.Update (Time.deltaTime)) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void Init(float timeTillDeath){
+		deathTimer.Activate (timeTillDeath);
+	}
+}
