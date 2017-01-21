5ddade719c25799b3c7c8212f5c3021c61674e58
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2017-01-21 09:50:49 -0800

    Added a few UI things

---
+++ Assets/Scripts/UI/TileSelectionGeneration.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileSelectionGeneration : MonoBehaviour {
+
+    public List<Transform> UiOptions;
+    public GraphMaker My_GraphMaker;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
