5cdaeb28b7fbdca617c845a729d5d94f1af102f1
christinejohnson <christinejohnson@nevada.unr.edu>
2017-01-21 11:38:56 -0800

    starting traps

---
+++ Assets/Scripts/Traps/PidePiper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PidePiper : Trap {
+
+	private GraphMaker graph;
+
+	// Use this for initialization
+	void Start () {
+		graph = GameObject.FindGameObjectWithTag ("GameBoard");
+	}
+
+	public override void ApplyTriggerEffect (){
+		// spawn some rats and plague in surounding 3 by 3 grid
+
+
+	}
+}
---
+++ Assets/Scripts/Traps/Trap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Trap : MonoBehaviour {
+
+	protected KeyCode triggerKey;
+	protected Vector2 gridPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (triggerKey)) {
+			ApplyTriggerEffect ();
+		}
+	}
+
+	public void DisplayTriggerKey(){
+		// TODO print text over the position showing the hotkey
+	}
+
+	public void Init(KeyCode key, Vector2 gridPosition){
+		triggerKey = key;
+		this.gridPosition = gridPosition;
+	}
+
+	public abstract void ApplyTriggerEffect ();
+}
