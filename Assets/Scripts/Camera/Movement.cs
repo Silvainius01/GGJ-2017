89cd6993e6d8c318d40296bb6d04bea4b46172bf
Andrew C Ward <andrew.ward@students.aie.edu.au>
2017-01-21 15:13:18 -0800

    Adding gyrocopter assets

---
+++ Assets/Scripts/Camera/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+	public Vector2 scrollLimit;
+
+	void Start () {
+	
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float xAxisValue = Input.GetAxis("Horizontal");
+		float yAxisValue = Input.GetAxis("Vertical");
+		float zAxisValue = Input.GetAxis ("Mouse ScrollWheel");
+		if(Camera.current != null && (Camera.current.transform.position.x <= scrollLimit.x && Camera.current.transform.position.x >= -scrollLimit.x && Camera.current.transform.position.y <= scrollLimit.y && Camera.current.transform.position.y >= -scrollLimit.y   ) )
+		{ 
+			Camera.current.transform.Translate(new Vector3(xAxisValue, yAxisValue, 0));
+		}
+
+		if (transform.position.x >= scrollLimit.x)
+			transform.position = new Vector3 (scrollLimit.x, transform.position.y, transform.position.z);
+		if (transform.position.x <= -scrollLimit.x)
+			transform.position = new Vector3 (-scrollLimit.x, transform.position.y, transform.position.z);
+		if (transform.position.y >= scrollLimit.y)
+			transform.position = new Vector3 (transform.position.x, scrollLimit.y, transform.position.z);
+		if (transform.position.y <= -scrollLimit.y)
+			transform.position = new Vector3 (transform.position.x, -scrollLimit.y, transform.position.z);
+	
+	}
+}
