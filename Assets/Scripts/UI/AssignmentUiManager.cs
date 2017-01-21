45dc269e5c08e91746b69d71158d8b03625706da
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2017-01-21 14:37:41 -0800

    Nearly done with UiCode

--- Assets/Scripts/UI/AssignmentUiManager.cs
+++ Assets/Scripts/UI/AssignmentUiManager.cs
@@ -6,10 +6,13 @@ public class AssignmentUiManager : MonoBehaviour {
 
     public static AssignmentUiManager Instance;
 
-    public List<ConstructionButton> ConstructButtons = new List<ConstructionButton>();
+    public List<GameObject> BuildingButtons = new List<GameObject>();
+    public List<GameObject> TrapButtons = new List<GameObject>();
+    public float UiRadius = 1f;
 
+    const float uiDistanceFromZero = -5;
     bool Active = false;
-    GameObject Tile;
+    GraphMaker.GraphPoint curTile;
     
     void Awake()
     {
@@ -24,5 +27,33 @@ public class AssignmentUiManager : MonoBehaviour {
         // assign a construct to a space
     }
 
-    //public void ActivateUI (GameObject _tile, List<ConstructionButton> _constructionOptions)
+    public void ActivateUI (GraphMaker.GraphPoint _gridPoint)
+    {
+        curTile = _gridPoint;
+        Active = true;
+
+        if(curTile.gridType == GraphMaker.GRID_TYPE.EMPTY_BUILDING)
+        {
+            PopulateUiRing(BuildingButtons);
+        } else if (curTile.gridType == GraphMaker.GRID_TYPE.TRAP)
+        {
+            PopulateUiRing(TrapButtons);
+        } else
+        {
+            Debug.Log("This space type cannot be populated");
+        }
+    }
+
+    void PopulateUiRing(List<GameObject> _toSpawn)
+    {
+        float _theta = 360 / _toSpawn.Count;
+        for (int i = 0; i < _toSpawn.Count; i++)
+        {
+            float _degreeItteration = _theta * i;
+            Vector2 _2dPoint = new Vector2(Mathf.Cos(_theta), Mathf.Sin(_theta)) * UiRadius;
+            Vector3 _spawnPoint = new Vector3(_2dPoint.x, _2dPoint.y, uiDistanceFromZero);
+            GameObject _uiElement = (GameObject)Instantiate(_toSpawn[i], _spawnPoint, Quaternion.identity);
+
+        }
+    }
 }
