7f1b82009f059310f9f84c88fb909ad921fcdb9c
Andrew C Ward <andrew.ward@students.aie.edu.au>
2017-01-22 11:17:08 -0800

    Added camera zoom

--- Assets/Scripts/Camera/Movement.cs
+++ Assets/Scripts/Camera/Movement.cs
@@ -3,23 +3,30 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-
+    //Camera Scroll
+    float xAxisValue;
+    float yAxisValue;
+    float zAxisValue;
 	public Vector2 scrollLimit;
 
+    //Camera Zoom
+    public float minFov = 10.0f;
+    public float maxFov = 30.0f;
+    public float zoomSensitivity = 10.0f;
+
 	void Start () {
-	
-
-
-
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		float xAxisValue = Input.GetAxis("Horizontal");
-		float yAxisValue = Input.GetAxis("Vertical");
-		float zAxisValue = Input.GetAxis ("Mouse ScrollWheel");
-		if(Camera.current != null && (Camera.current.transform.position.x <= scrollLimit.x && Camera.current.transform.position.x >= -scrollLimit.x && Camera.current.transform.position.y <= scrollLimit.y && Camera.current.transform.position.y >= -scrollLimit.y   ) )
+
+        xAxisValue = Input.GetAxis("Horizontal");
+        yAxisValue = Input.GetAxis("Vertical");
+        zAxisValue = -Input.GetAxis("Mouse ScrollWheel");
+        float fov = Camera.main.orthographicSize;
+
+        if (Camera.current != null && (Camera.current.transform.position.x <= scrollLimit.x && Camera.current.transform.position.x >= -scrollLimit.x && Camera.current.transform.position.y <= scrollLimit.y && Camera.current.transform.position.y >= -scrollLimit.y   ) )
 		{ 
 			Camera.current.transform.Translate(new Vector3(xAxisValue, yAxisValue, 0));
 		}
@@ -32,6 +39,9 @@ public class Movement : MonoBehaviour {
 			transform.position = new Vector3 (transform.position.x, scrollLimit.y, transform.position.z);
 		if (transform.position.y <= -scrollLimit.y)
 			transform.position = new Vector3 (transform.position.x, -scrollLimit.y, transform.position.z);
-	
+
+        fov += zAxisValue * zoomSensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.orthographicSize = fov;
 	}
 }
