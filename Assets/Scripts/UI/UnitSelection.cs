8162804560701ab6b50b85c6d31862b07e366286
Connor Adam <Connor Adam>
2017-01-21 23:23:27 -0800

    Yay selection

--- Assets/Scripts/UI/UnitSelection.cs
+++ Assets/Scripts/UI/UnitSelection.cs
@@ -8,44 +8,59 @@ public class UnitSelection : MonoBehaviour
     public Vector2 fPos;
     public Texture2D texture;
     public  Rect rect = new Rect();
+
+    LineRenderer lines;
 	// Use this for initialization
 	void Start ()
     {
-	
+        lines = GetComponent<LineRenderer>();
+
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-    { 
-	    if(Input.GetMouseButton(0))
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
         {
             fPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            if(first)
+            if (first)
             {
                 sPos = fPos;
                 first = false;
+                lines.SetPosition(0, sPos);
+                lines.SetPosition(4, sPos);
+                lines.enabled = true;
             }
-            rect = new Rect(Mathc.GetMidPoint(sPos, fPos), new Vector2(10.0f, 10.0f));
+
+            lines.SetPosition(1, new Vector2(sPos.x, fPos.y));
+            lines.SetPosition(2, fPos);
+            lines.SetPosition(3, new Vector2(fPos.x, sPos.y));
         }
-        else if(!first)
+        else if (!first)
         {
             var unitsOnMap = FindObjectsOfType<Unit>();
 
-            foreach(var unit in unitsOnMap)
-                if (Mathc.VectorIsBetween(unit.transform.position, sPos, fPos))
-                    if (unit.ownedByPlayer)
+            foreach (var unit in unitsOnMap)
+                if (unit.ownedByPlayer)
+                {
+                    if (Mathc.VectorIsBetween(unit.transform.position, sPos, fPos))
                         unit.isSelected = true;
+                    else
+                        unit.isSelected = false;
+                }
             first = true;
+
+            lines.enabled = false;
         }
-	}
+    }
 
     private void OnGUI()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GUI.color = Color.red;
-            GUI.DrawTexture(rect, texture);
+            // GUI.color = Color.red;
+            // GUI.DrawTexture(rect, texture);
         }
     }
 }
