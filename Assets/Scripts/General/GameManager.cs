06f56ea146d7f7a5d162012492b766b01a22ca48
christinejohnson <christinejohnson@nevada.unr.edu>
2017-01-21 22:00:40 -0800

    starting game manager

---
+++ Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	enum GameState{
+		BUILD,
+		PLAY
+	}
+
+	private GameState state = GameState.BUILD;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
